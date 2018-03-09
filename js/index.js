4743867812c3f08835954bb41330f6d13e60eafa
sanr4-15 <32267189+sanr4-15@users.noreply.github.com>
2018-03-09 16:04:23 +0000

    added a for loop

--- js/index.js
+++ js/index.js
@@ -5,15 +5,30 @@ $(document).on('pagecreate', '#feedPage', function(event) {
 	
 	// Use an HTML GET request to obtain data from an API
 	var xmlhttp=new XMLHttpRequest();
-	xmlhttp.open("GET", feedURL, false);
+	xmlhttp.open("GET", feedURL, true);
 	xmlhttp.send();
 		
-		
+	xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     
+    
+    
 	// parse the resulting JSON into Javascript Data Object 
 	// you can use a live parser to inspect the contents of the JSON
 	// http://json.parser.online.fr/ 
 	var weather= JSON.parse(xmlhttp.responseText);
 	
+    weather.consolidated_weather[0].min_temp;  
+    
+        
+    var sum = 0;
+    for (var i =0; i<6; i=i+1) {
+        
+        console.log(weather.consolidated_weather[i].min_temp);
+        
+    }
+        
+        
 	
 	//Define Ractive binding
 	var ractive = new Ractive({
@@ -21,5 +36,6 @@ $(document).on('pagecreate', '#feedPage', function(event) {
     	template: '#myTemplate', <!-- how -->
     	data: { weather : weather.consolidated_weather } <!-- what - specify the list of weather reports using dot notation-->
 	});
-	
+    }
+    };
 });
